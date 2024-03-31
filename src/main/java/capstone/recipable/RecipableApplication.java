c9517bb312b8f0abe9afc7a7ef4273b3d02aa4dc
chahyunsoo <chahs1012@naver.com>
2024-03-31 13:09:48 +0900

    [feat] : Initial Commit

---
+++ src/main/java/capstone/recipable/RecipableApplication.java
@@ -0,0 +1,13 @@
+package capstone.recipable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/recipable/RecipableApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.recipable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
