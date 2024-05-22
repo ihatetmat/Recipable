2670df366eac5e4e974cd5cfb2890fe1f4eab09a
Jeongho <fgh2585@gmail.com>
2024-05-22 11:30:44 +0900

    Docs: youtube 관련 swagger

--- src/main/java/capstone/recipable/domain/youtube/controller/YoutubeController.java
+++ src/main/java/capstone/recipable/domain/youtube/controller/YoutubeController.java
@@ -2,6 +2,8 @@ package capstone.recipable.domain.youtube.controller;
 
 import capstone.recipable.domain.youtube.dto.response.YoutubeResponse;
 import capstone.recipable.domain.youtube.service.YoutubeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,17 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/youtube")
+@Tag(name = "youtube", description = "youtube 관련 api")
 public class YoutubeController {
 
     private final YoutubeService youtubeService;
 
+    @Operation(summary = "유튜브 api", description = """
+            유튜브 영상 첨부 api입니다.
+            
+            param으로 keyword를 보내주시면 유튜브에서 검색하여 영상 3개를 반환해줍니다.
+            
+            """)
     @GetMapping
     public ResponseEntity<List<YoutubeResponse>> searchVideo(@RequestParam String keyword) throws IOException {
         List<YoutubeResponse> results = youtubeService.searchVideo(keyword);
