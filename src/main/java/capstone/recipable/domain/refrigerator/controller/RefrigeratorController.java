e84ba7e34bdbe6eb757e018497fe56c72d38414b
chahyunsoo <chahs1012@naver.com>
2024-05-27 14:32:04 +0900

    [refactor] : 냉장고 안 식재료 상세 조회 메소드 오타 수정

--- src/main/java/capstone/recipable/domain/refrigerator/controller/RefrigeratorController.java
+++ src/main/java/capstone/recipable/domain/refrigerator/controller/RefrigeratorController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,7 +34,7 @@ public class RefrigeratorController {
                         
             """)
     @GetMapping("/{ingredientId}")
-    public ResponseEntity<SuccessResponse<IngredientDetailResponse>> getAllIngredientsByRefrigerator(@PathVariable Long ingredientId) {
+    public ResponseEntity<SuccessResponse<IngredientDetailResponse>> getIngredients(@PathVariable Long ingredientId) {
         IngredientDetailResponse ingredientDetail = refrigeratorService.getIngredient(ingredientId);
         return SuccessResponse.of(ingredientDetail);
     }
