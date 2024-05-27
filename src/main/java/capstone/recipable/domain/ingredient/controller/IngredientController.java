c71d8c3e852ffe84c8149df442437216ff79b523
chahyunsoo <chahs1012@naver.com>
2024-05-27 12:07:28 +0900

    [refactor] : 영수증 분석 api swagger 수정

--- src/main/java/capstone/recipable/domain/ingredient/controller/IngredientController.java
+++ src/main/java/capstone/recipable/domain/ingredient/controller/IngredientController.java
@@ -4,7 +4,10 @@ import capstone.recipable.domain.ingredient.controller.dto.response.IngredientLi
 import capstone.recipable.domain.ingredient.service.NaverOcrService;
 import capstone.recipable.domain.ingredient.service.PredictService;
 import capstone.recipable.global.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -15,13 +18,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "ingredient", description = "영수증 분석 및 재료 관련 api")
 public class IngredientController {
     private final NaverOcrService naverOcrService;
     private final PredictService predictService;
 
-    @PostMapping("/receipt")
+    @Operation(summary = "영수증 업로드 후 재료 카테고리 및 재료 이름 반환 api", description = """
+            form/data 형식으로 영수증 이미지를 업로드 하면
+            
+            상품명 텍스트 추출 후 재료 카테고리, 재료 이름을 반환해줍니다.
+            
+            """)
+    @PostMapping(value = "/receipt", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<SuccessResponse<IngredientListResponse>> getIngredientsByOcr(@RequestPart(value = "multipartFile")
-                                                                   MultipartFile multipartFile) {
+                                                                                       MultipartFile multipartFile) {
 
         List<String> ingredientList = naverOcrService.callApi(multipartFile);
         IngredientListResponse ingredientListResponse = predictService.returnPrediction(ingredientList);
