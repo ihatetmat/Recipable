2076d1e67818203723819170b9f3cc66da5e63c6
chahyunsoo <chahs1012@naver.com>
2024-05-28 15:30:48 +0900

    [feat] : 이미지 업로드 메소드 추가

---
+++ src/main/java/capstone/recipable/global/s3/AmazonS3Manager.java
@@ -0,0 +1,40 @@
+package capstone.recipable.global.s3;
+
+import capstone.recipable.global.entity.Uuid;
+import capstone.recipable.global.error.ApplicationException;
+import capstone.recipable.global.error.ErrorCode;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AmazonS3Manager{
+
+    private final AmazonS3 amazonS3;
+
+    private final AmazonConfig amazonConfig;
+
+    private final UuidRepository uuidRepository;
+    public String uploadFile(String keyName, MultipartFile file) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        try {
+            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(), keyName, file.getInputStream(), metadata));
+        }catch (IOException e) {
+            throw new ApplicationException(ErrorCode.IMAGE_IS_NOT_UPLOADED);
+        }
+        return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+    }
+
+    public String generateIngredientKeyName(Uuid uuid) {
+        return amazonConfig.getIngredientPath() + '/' + uuid.getUuid();
+    }
+}
