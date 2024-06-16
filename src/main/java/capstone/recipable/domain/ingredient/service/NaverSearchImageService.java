8fdf04e9713ae893d893e5b8a30da3b840e7a113
chahyunsoo <chahs1012@naver.com>
2024-06-16 19:04:38 +0900

    [refactor] : naver image api 이미지 크기 조정

--- src/main/java/capstone/recipable/domain/ingredient/service/NaverSearchImageService.java
+++ src/main/java/capstone/recipable/domain/ingredient/service/NaverSearchImageService.java
@@ -29,7 +29,7 @@ public class NaverSearchImageService {
                 .queryParam("display", 1)
                 .queryParam("start", 1)
                 .queryParam("sort", "sim")
-                .queryParam("filter","medium")
+                .queryParam("filter","large")
                 .encode(Charset.forName("UTF-8"))
                 .build()
                 .toUri();
